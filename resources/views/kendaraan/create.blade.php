4c4b43d6ae7870e0f043663d91344e9b68332eb3
pokiro <muhammadanjothosatru@gmail.com>
2022-09-01 11:59:50 +0700

    detail pelanggan + form kendaraan

---
+++ resources/views/kendaraan/create.blade.php
@@ -0,0 +1,68 @@
+@extends('template.master')
+@section('link_css')
+<link rel="stylesheet" href="{{ asset('css/pelanggan-create.css')}}">
+@endsection
+@section('konten')
+<div class="card">
+    @if(count($errors)>0)
+  	@foreach($errors->all() as $error)
+  	<div class="alert alert-danger" role="alert">
+      {{ $error }}
+	</div>  		
+  	@endforeach
+  @endif
+
+  @if(Session::has('success'))
+  	<div class="alert alert-success" role="alert">
+      {{ Session('success') }}
+	</div> 
+  	
+  @endif
+    <form action="{{ route('pelanggan.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+    <div class="m-4">
+        <div class="row pl-0 pr-0">
+            <div class="font-form-header mb-3 col-6">Masukkan Detail Kendaraan</div>
+            <div class="font-form-header mb-3 col-6 d-flex justify-content-end">
+                <a href="{{ route('pelanggan.index') }}" class="btn btn-primary btn-sm mr-2"><i class="fas fa-arrow-left mr-2"></i>Kembali</a>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-6">
+                <div class="mb-3 row">
+                    <label for="inputNoPol"  class="col-sm-2 col-form-label font-form">No. Pol.</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="nopol" value="{{ old('nik') }}" required="required" class="form-control form-control-size" placeholder="Masukkan Nomor Polisi" id="nopol">
+                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputNamaPemilik"  class="col-sm-2 col-form-label font-form">Nama Pemilik</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="namapemilik" value="{{ old('nama') }}" required="required" class="form-control form-control-size" placeholder="Masukkan Nama Pemilik" id="namapemilik">
+                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputNama" class="col-sm-2 col-form-label font-form">Alamat</label>
+                    <div class="form-floating col-sm-10">
+                        <textarea class="form-control textarea-control-size" required="required" name="alamat" placeholder="Masukkan Alamat Pemilik" id="alamat">{{ old('alamat') }}</textarea>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputNama" class="col-sm-2 col-form-label font-form">No. HP</label>
+                        <div class="col-sm-10">
+                            <div class="btn-group">
+                                <button class="btn dropdown-toggle" type="button" id="defaultDropdown" data-bs-toggle="dropdown" data-bs-auto-close="true" aria-expanded="false">
+                                    Pilih Merk
+                                </button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+   
+    </div>
+</form>
+</div>
+
+@endsection
---
+++ resources/views/pelanggan/edit.blade.php
@@ -0,0 +1,71 @@
+@extends('template.master')
+@section('link_css')
+<link rel="stylesheet" href="{{ asset('css/edit-pelanggan.css')}}">
+@endsection
+@section('konten')
+<div class="card">
+    @if(count($errors)>0)
+  	@foreach($errors->all() as $error)
+  	<div class="alert alert-danger" role="alert">
+      {{ $error }}
+	</div>  		
+  	@endforeach
+  @endif
+
+  @if(Session::has('success'))
+  	<div class="alert alert-success" role="alert">
+      {{ Session('success') }}
+	</div> 
+  	
+  @endif
+    <form action="{{ route('pelanggan.edit', $pelanggan->id) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+    <div class="m-4">
+        <div class="row pl-0 pr-0">
+            <div class="font-form-header mb-3 col-6">Detail Pelanggan</div>
+            <div class="font-form-header mb-3 col-6 d-flex justify-content-end">
+                <a href="{{ route('pelanggan.index') }}" class="btn btn-primary btn-sm mr-2"><i class="fas fa-arrow-left mr-2"></i>Kembali</a>
+                <a href="{{ route('pelanggan.index') }}" class="btn btn-primary btn-sm mr-2"><i class="fas fa-pen mr-2"></i>Ubah</a>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-6">
+                <div class="mb-3 row">
+                    <label for="inputNIK"  class="col-sm-2 col-form-label font-form">NIK</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly class="form-control-plaintext font-data" id="staticEmail" value="3525051806000004">
+                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputNama"  class="col-sm-2 col-form-label font-form">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly class="form-control-plaintext font-data" id="staticEmail" value="Muhammad Anjotho Satru">
+                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputNama" class="col-sm-2 col-form-label font-form">Alamat</label>
+                    <div class="form-floating col-sm-10">
+                        <textarea rows="4" class="form-control-plaintext font-data" readonly>Kaliombo, RT.011/RW.003, Tambakrejo, Duduksampeyan, Gresik, Jawa Timur, Indonesia</textarea>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputNama" class="col-sm-2 col-form-label font-form">No. HP</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly class="form-control-plaintext font-data" id="staticEmail" value="081217728534">
+                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="file-upload" class="col-sm-2 col-form-label font-form">Foto KTP</label>
+                    <div class="form-floating col-sm-10">
+                        <img src="{{asset('images/Muhammad Anjotho Satru-1662002186.jpeg')}}" class="foto-ktp" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+   
+    </div>
+</form>
+</div>
+
+@endsection
