77d1294328c2b655ac40e002d98441c9353948d4
alvianyulianto07 <alvianyulianto07@gmail.com>
2022-09-06 21:11:07 +0700

    create kendaraan

--- app/Http/Controllers/KendaraanController.php
+++ app/Http/Controllers/KendaraanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Kendaraan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class KendaraanController extends Controller
 {
@@ -36,7 +37,50 @@ class KendaraanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate= $request->validate([
+            'no_pol' => 'required|unique:kendaraan',
+            'nama_pemilik' => 'required',
+            'alamat' => 'required',
+            'merk' => 'required',
+            'tipe' => 'required',
+            'jenis' => 'required',
+            'model' => 'required',
+            'tahun_pembuatan' => 'required|numeric',
+            'daya_listrik' => 'required',
+            'no_rangka' => 'required',
+            'no_mesin' => 'required',
+            'warna' => 'required',
+            'tahun_registrasi' => 'required',
+            'no_bpkb' => 'required',
+            'harga_beli' => 'required|numeric',
+            'tanggal_masuk' => 'required',
+        
+        ]);
+        $kendaraan = Kendaraan::create([
+            'no_pol' => $request->no_pol,
+            'users_id' => Auth::user()->id,
+            'nama_pemilik' => $request->nama_pemilik,
+            'alamat' => $request->alamat,
+            'merk' => $request->merk,
+            'tipe' => $request->tipe,
+            'jenis' => $request->jenis,
+            'model' => $request->model,
+            'tahun_pembuatan' => $request->tahun_pembuatan,
+            'daya_listrik' => $request->daya_listrik,
+            'no_rangka' => $request->no_rangka,
+            'no_mesin' => $request->no_mesin,
+            'warna' => $request->warna,
+            'status_kendaraan' =>"Tersedia",
+            'cabang' =>Auth::user()->cabang,
+            'tahun_registrasi' => $request->tahun_registrasi,
+            'no_bpkb' => $request->no_bpkb,
+            'harga_beli' => $request->harga_beli,
+            'tanggal_masuk' => $request->tanggal_masuk,
+            'supplier' => $request->supplier,
+            'keterangan' => $request->keterangan,
+        ]);
+        return redirect('/kendaraan')->with('success','data berhasil ditambahkan');
+            
     }
 
     /**
