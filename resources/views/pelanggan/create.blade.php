be000a169c7332ce992dd3b438cc395f41285445
muhammadanjothosatru <muhammadanjothosatru@gmail.com>
2022-09-15 20:51:44 +0700

    alert dismissible

--- resources/views/pelanggan/create.blade.php
+++ resources/views/pelanggan/create.blade.php
@@ -6,7 +6,7 @@
 <div class="card mt-4">
     @if(count($errors)>0)
   	@foreach($errors->all() as $error)
-  	<div class="alert alert-danger" role="alert">
+  	<div class="alert alert-danger alert-dismissible fade show" role="alert">
       {{ $error }}
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
