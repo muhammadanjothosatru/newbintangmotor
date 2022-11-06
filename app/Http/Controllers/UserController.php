61f91ce1e5f3adf4e3b74807be79e85e7a4741fb
alvianyulianto07 <alvianyulianto07@gmail.com>
2022-11-06 22:01:21 +0700

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -125,6 +125,17 @@ class UserController extends Controller
         $user = User::find(Auth: id());
         $user->password = Hash::make($request->password_baru);
         $user ->save();
+        
+        // #match The Old Password
+        // if(!Hash::check($request->old_password, auth()->user()->password)){
+        //     return back()->with("error", "Old Password Doesn't match!");
+        // }
+
+
+        // #Update the new Password
+        // User::whereId(auth()->user()->id)->update([
+        //     'password' => Hash::make($request->new_password)
+        // ]);
         return back()->with("status", "Password changed successfully!");
     }
 
