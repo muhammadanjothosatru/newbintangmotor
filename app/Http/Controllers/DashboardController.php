1311871da251d2b902f27bb365c4f2557301b1e1
Muhammad Luthfi <mluthfi3d@gmail.com>
2022-11-07 12:38:22 +0700

    fix dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -77,18 +77,18 @@ class DashboardController extends Controller
                         ->where('kendaraan.jenis', '=', 'Sepeda Motor')
                         ->whereMonth('transaksi.created_at', $month)
                         ->where('kendaraan.status_kendaraan', '=', 'Terjual')
-                        ->select(DB::raw('sum(harga_akhir - harga_beli - komisi) as total_keuntungan'));
+                        ->select(DB::raw('sum(transaksi.harga_akhir - (kendaraan.harga_beli + transaksi.komisi)) as total_keuntungan'));
                     } else if (Auth::user()->role == 2) {
                         $keuntungan->where('users.cabang_id', Auth::user()->cabang_id)
                         ->where('kendaraan.jenis', '=', 'Mobil')
                         ->whereMonth('transaksi.created_at', $month)
                         ->where('kendaraan.status_kendaraan', '=', 'Terjual')
-                        ->select(DB::raw('sum(harga_akhir - harga_beli - komisi) as total_keuntungan'));
+                        ->select(DB::raw('sum(transaksi.harga_akhir - (kendaraan.harga_beli + transaksi.komisi)) as total_keuntungan'));
                     } else if (Auth::user()->role == 0) {
                         $keuntungan
                         ->whereMonth('transaksi.created_at', $month)
                         ->where('kendaraan.status_kendaraan', '=', 'Terjual')
-                        ->select(DB::raw('sum(harga_akhir - harga_beli - komisi) as total_keuntungan'));
+                        ->select(DB::raw('sum(transaksi.harga_akhir - (kendaraan.harga_beli + transaksi.komisi)) as total_keuntungan'));
                     }
                     $all_keuntungan=$keuntungan->get();
 
