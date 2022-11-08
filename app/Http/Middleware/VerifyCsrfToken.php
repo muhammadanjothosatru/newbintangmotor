3bbaa4fb33544636b3f1812102a12ed41e9bb6e3
muhammadanjothosatru <muhammadanjothosatru@gmail.com>
2022-11-08 09:38:14 +0700

    .

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'bintangmotor.herokuapp.com/login'
+        'bintangmotor.herokuapp.com/*'
     ];
 }
